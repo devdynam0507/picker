fc38809e87610f4e5401cd4c33a86150d91faf98
devdynam0507 <wsnam0507@naver.com>
2021-12-15 01:57:07 +0900

    feat: Dockerfile, nginx.conf, build 스크립트 작성

---
+++ build.sh
@@ -0,0 +1,14 @@
+echo "기존에 생성된 이미지가 있다면 삭제합니다"
+# 이미지 삭제
+docker rmi dynam0507/picker-fe:0.1 || notExistsImage=true
+
+if [ $notExistsImage ]
+then
+    echo "기존에 생성된 이미지가 없습니다."
+fi
+
+echo "이전 프로세스에 대해 이상이 없습니다."
+echo "이제 이미지를 만들고 컨테이너를 띄웁니다."
+
+echo `docker build -t dynam0507/picker-fe:0.1 .`
+echo `docker login -u dynam0507 -p xxxx`
