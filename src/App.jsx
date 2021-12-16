e7e8af5473340f99f7e1f50e20c89393625aa3c0
ByungyeonKim <viva_kby@naver.com>
2021-12-16 10:15:44 +0900

    파일명 통일: 소문자 -> 파스칼케이스

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import './App.css';
+import Home from './pages/Home';
+import Category from './pages/Category';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route exact path='/' element={<Home />} />
+        <Route path=':category' element={<Category />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
