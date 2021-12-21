4b155b18f7d74cf1e4a6a3066af02c7aadbf0cb0
ByungyeonKim <viva_kby@naver.com>
2021-12-21 15:04:40 +0900

    Home.jsx: 현재위치 로딩화면 구현, 스타일 추가

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -8,29 +8,29 @@ const Home = () => {
   const [loc, setLoc] = useState('');
   const [numbVisit, setNumbVisit] = useState(0);
   const [curList, setCurList] = useState([]);
+  const [isLoading, setLoading] = useState(true);
+
   useEffect(() => {
     axios.get('http://api.picker.run/count').then((res) => {
       setNumbVisit(res.data.count);
     });
-    axios
-      .get('http://api.picker.run/picker?amountOfData=6')
-      .then((res) => {
-        console.log(res.data);
-        setCurList(res.data);
-      });
+    axios.get('http://api.picker.run/picker?amountOfData=6').then((res) => {
+      console.log(res.data);
+      setCurList(res.data);
+    });
   }, []);
+
   const getLocation = () => {
     if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition(
-        function (position) {
-          console.log(
-            position.coords.latitude + ' ' + position.coords.longitude
-          );
-          setLoc(
-            position.coords.latitude + ' ' + position.coords.longitude
-          );
+        (position) => {
+          const currentPosition =
+            position.coords.latitude + ' ' + position.coords.longitude;
+          console.log(currentPosition);
+          setLoading(false);
+          setLoc(currentPosition);
         },
-        function (error) {
+        (error) => {
           console.error(error);
         },
         {
@@ -62,66 +62,77 @@ const Home = () => {
           </div>
         </div>
       </section> */}
-      <div className='wapper'>
-        {/* {curList.map((el) => el.local + '/' + el.food + ' ')} */}
-        <h1 className='tit'>Picker</h1>
-        <p className='txt'>
-          Picker가 {numbVisit}명의 고민을 해결해줬어요.
-        </p>
-        <p className='txt'>원하는 고민거리를 Picker 해드릴게요.</p>
-        <p className='nemu'>메뉴 Picker</p>
-        <ul className='inner'>
-          <li className='item'>
-            <Link to='/kr' state={{ loc: loc, theme: '한식' }}>
-              한식
-            </Link>
-          </li>
-          <li className='item'>
-            <Link to='/jp' state={{ loc: loc, theme: '일식' }}>
-              일식
-            </Link>
-          </li>
-          <li className='item'>
-            <Link to='/cn' state={{ loc: loc, theme: '중식' }}>
-              중식
-            </Link>
-          </li>
-        </ul>
-        <ul className='inner'>
-          <li className='item'>
-            <Link to='/meat' state={{ loc: loc, theme: '고기' }}>
-              고기
-            </Link>
-          </li>
-          <li className='item'>
-            <Link to='/chicken' state={{ loc: loc, theme: '치킨' }}>
-              치킨
-            </Link>
-          </li>
-          <li className='item'>
-            <Link to='/pizza' state={{ loc: loc, theme: '피자' }}>
-              피자
-            </Link>
-          </li>
-        </ul>
-        <ul className='inner'>
-          <li className='item'>
-            <Link to='/snack' state={{ loc: loc, theme: '분식' }}>
-              분식
-            </Link>
-          </li>
-          <li className='item'>
-            <Link to='/midnightsnack' state={{ loc: loc, theme: '야식' }}>
-              야식
-            </Link>
-          </li>
-          <li className='item'>
-            <Link to='/ramen' state={{ loc: loc, theme: '라면' }}>
-              라면
-            </Link>
-          </li>
-        </ul>
-      </div>
+      {isLoading && (
+        <section className='loader-section'>
+          <div className='loader'>
+            <div className='upper ball'></div>
+            <div className='right ball'></div>
+            <div className='lower ball'></div>
+            <div className='left ball'></div>
+          </div>
+          <p className='loader-text'>지도를 불러오는 중이에요..</p>
+        </section>
+      )}
+      {!isLoading && (
+        <div className='wapper'>
+          {/* {curList.map((el) => el.local + '/' + el.food + ' ')} */}
+          <h1 className='tit'>Picker</h1>
+          <p className='txt'>Picker가 {numbVisit}명의 고민을 해결해줬어요.</p>
+          <p className='txt'>원하는 고민거리를 Picker 해드릴게요.</p>
+          <p className='nemu'>메뉴 Picker</p>
+          <ul className='inner'>
+            <li className='item'>
+              <Link to='/kr' state={{ loc: loc, theme: '한식' }}>
+                한식
+              </Link>
+            </li>
+            <li className='item'>
+              <Link to='/jp' state={{ loc: loc, theme: '일식' }}>
+                일식
+              </Link>
+            </li>
+            <li className='item'>
+              <Link to='/cn' state={{ loc: loc, theme: '중식' }}>
+                중식
+              </Link>
+            </li>
+          </ul>
+          <ul className='inner'>
+            <li className='item'>
+              <Link to='/meat' state={{ loc: loc, theme: '고기' }}>
+                고기
+              </Link>
+            </li>
+            <li className='item'>
+              <Link to='/chicken' state={{ loc: loc, theme: '치킨' }}>
+                치킨
+              </Link>
+            </li>
+            <li className='item'>
+              <Link to='/pizza' state={{ loc: loc, theme: '피자' }}>
+                피자
+              </Link>
+            </li>
+          </ul>
+          <ul className='inner'>
+            <li className='item'>
+              <Link to='/snack' state={{ loc: loc, theme: '분식' }}>
+                분식
+              </Link>
+            </li>
+            <li className='item'>
+              <Link to='/midnightsnack' state={{ loc: loc, theme: '야식' }}>
+                야식
+              </Link>
+            </li>
+            <li className='item'>
+              <Link to='/ramen' state={{ loc: loc, theme: '라면' }}>
+                라면
+              </Link>
+            </li>
+          </ul>
+        </div>
+      )}
     </main>
   );
 };
