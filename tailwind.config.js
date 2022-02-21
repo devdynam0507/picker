f0bbf48ebf402013b8687a8fb4324194a16d8436
ByungyeonKim <viva_kby@naver.com>
2022-02-21 22:30:58 +0900

    ADD: Tailwind config extend 추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,22 @@
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'picker-orange': '#f45314',
+        'picker-yellow': '#ffcc33',
+      },
+      backgroundImage: {
+        'top-bg1': "url('/img/top-bg1.png')",
+        'top-bg2': "url('/img/top-bg2.png')",
+      },
+      screens: {
+        tall: { raw: '(min-height: 750px)' },
+      },
+      flex: {
+        30: '1 1 30%',
+      },
+    },
   },
   plugins: [],
 };
